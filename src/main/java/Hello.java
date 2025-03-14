a611952ac7f9229ff872c975137005ee540fe48b
kimhyengyu <sisalbkim@gmail.com>
2025-03-14 14:29:00 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!! My First Java Coding!!");
+        System.out.println("Power Overwhelming");
+        System.out.println("Black Sleep Wall");
+        System.out.println("Show Me The Money");
+        System.out.println("Operation Cwal");
+        System.out.println("The Gathering");
+        System.out.println("Play Zerg Radio");
+    }
+}
