200412c3a8b18a542ac6dc6c3722339f57fac5c3
kimhyengyu <sisalbkim@gmail.com>
2025-03-14 14:55:26 +0900

    ㅈㅈ

--- src/main/java/Hello2.java
+++ src/main/java/Hello2.java
@@ -2,7 +2,7 @@ public class Hello2 {
     public static void main(String[] args) {
 
         System.out.println("Hello World2");
-        System.out.println("Avengers asameble");
+        System.out.println("두번째");
 
     }
 }
